3905783c153a5c735879c49b836e777d7d957da7
Harshit <devInnow8@gmail.com>
2023-04-17 15:51:27 +0530

    initial set up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { Component } from "react";
-import { getWeb3 } from "./MetaMask";
+import React from "react";
 
-class App extends Component {
-  async componentDidMount() {
-    const web3 = await getWeb3();
-    console.log(web3, 'checkkk');
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, world!</h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
 
 export default App;
